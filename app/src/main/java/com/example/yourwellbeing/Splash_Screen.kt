517c688a404003ffd0de62bf44272edef3bb9d6c
atif9 <mohammadatif92080@gmail.com>
2020-12-16 18:53:46 +0530

    splash screen added

---
+++ app/src/main/java/com/example/yourwellbeing/Splash_Screen.kt
@@ -0,0 +1,23 @@
+package com.example.yourwellbeing
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash_Screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash__screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this,detail_taker_activity::class.java))
+            },2500
+        )
+
+    }
+}
